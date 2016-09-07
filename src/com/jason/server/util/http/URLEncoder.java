7c0bcf30a827cbd86d2b0fc2f1462c902dada9e9
jasongzcity <jason_lo@qq.com>
2016-09-07 14:48:10 +0800

    polish

--- src/com/jason/server/util/http/URLEncoder.java
+++ src/com/jason/server/util/http/URLEncoder.java
@@ -22,20 +22,20 @@ import java.util.BitSet;
  */
 public class URLEncoder
 {
-	//Notice: in DEFAULT encoder ,' '(space) will not be encoded as '+'
-	public static final URLEncoder DEFAULT = new URLEncoder();
-	static {
+    //Notice: in DEFAULT encoder ,' '(space) will not be encoded as '+'
+    public static final URLEncoder DEFAULT = new URLEncoder();
+    static {
         DEFAULT.addSafeCharacter('~');
         DEFAULT.addSafeCharacter('-');
         DEFAULT.addSafeCharacter('_');
         DEFAULT.addSafeCharacter('.');
         DEFAULT.addSafeCharacter('*');
         DEFAULT.addSafeCharacter('/');
-	}
+    }
 	
-	//Arrays for hexadecimal numbers mapping to characters
-	//upper case
-	public final static char[] HEXADECIMALS  = 
+    //Arrays for hexadecimal numbers mapping to characters
+    //upper case
+    public final static char[] HEXADECIMALS  = 
 						{'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 	
 	//definite safe character
@@ -51,63 +51,63 @@ public class URLEncoder
        }
     }
     
-	protected BitSet safeCharacters = new BitSet(256);
+    protected BitSet safeCharacters = new BitSet(256);
 	
-	/**
-	 * add safe character to skip encoding
-	 * @param c the safe character
-	 */
-	public void addSafeCharacter(char c) {
-		safeCharacters.set(c);
-	}
+    /**
+     * add safe character to skip encoding
+     * @param c the safe character
+     */
+    public void addSafeCharacter(char c) {
+        safeCharacters.set(c);
+    }
 	
-	/**
-	 * encode method 
-	 * @param src the source string
-	 * @param enc character encoding name
-	 * @return the URL-safe string
-	 */
-	public String encode(String src,String enc)
-	{
-	    boolean hasChange = false;
-	    Charset charset = null;
-		StringBuilder builder = new StringBuilder(src.length());
-		CharArrayWriter writer = new CharArrayWriter();
-		try {
-			 charset = Charset.forName(enc);
-		} catch(Exception e) {
-			charset = StandardCharsets.UTF_8;//default
-		}
-		
-		for(int i=0;i<src.length();) 
-		{
-			char c = src.charAt(i);
-			if(safeCharacters.get(c))
-			{
-			    builder.append(c);
-			    i++;
-			}
-			else
-			{
-			    hasChange = true;
-			    do
-			    {
-			        writer.append(c);
-			        i++;
-			    }while(i<src.length() && !safeCharacters.get(c = src.charAt(i)));
-			    
-			    //deal with not safe character
-			    byte[] bytes = writer.toString().getBytes(charset);
-			    for(int j=0;j<bytes.length;j++)
-			    {
-			        builder.append('%');
-			        builder.append(HEXADECIMALS[(bytes[j] & 0xF0) >> 4]);//get high 4bit
-			        builder.append(HEXADECIMALS[bytes[j] & 0x0F]);//low 4bit
-			    }
-			}
-		}
-		return hasChange ? builder.toString() : src;
-	}
+    /**
+     * encode method 
+     * @param src the source string
+     * @param enc character encoding name
+     * @return the URL-safe string
+     */
+    public String encode(String src,String enc)
+    {
+        boolean hasChange = false;
+        Charset charset = null;
+        StringBuilder builder = new StringBuilder(src.length());
+        CharArrayWriter writer = new CharArrayWriter();
+        try {
+            charset = Charset.forName(enc);
+        } catch(Exception e) {
+            charset = StandardCharsets.UTF_8;//default
+        }
+
+        for(int i=0;i<src.length();) 
+        {
+            char c = src.charAt(i);
+            if(safeCharacters.get(c))
+            {
+                builder.append(c);
+                i++;
+            }
+            else
+            {
+                hasChange = true;
+                do
+                {
+                    writer.append(c);
+                    i++;
+                }while(i<src.length() && !safeCharacters.get(c = src.charAt(i)));
+	    
+                //deal with not safe character
+                byte[] bytes = writer.toString().getBytes(charset);
+                for(int j=0;j<bytes.length;j++)
+                {
+                    builder.append('%');
+                    builder.append(HEXADECIMALS[(bytes[j] & 0xF0) >> 4]);//get high 4bit
+                    builder.append(HEXADECIMALS[bytes[j] & 0x0F]);//low 4bit
+                }
+            }
+        }
+        return hasChange ? builder.toString() : src;
+    }
 	
 	//for test
 //	public static void main(String[] args) throws UnsupportedEncodingException
